b277ba65433088e1220ca7290fba804f71dbcba4
Kabir Khan <kakh@microsoft.com>
2019-02-09 11:51:51 -0800

    Fix env file name

--- write_env.py
+++ write_env.py
@@ -2,7 +2,7 @@ import os
 
 
 if __name__ == '__main__':
-    with open('.envasdf', 'w+') as env_file:
+    with open('.env', 'w+') as env_file:
         for k, v in os.environ.items():
             if k.startswith('AZURE') or k.startswith('COSMOS') or k.startswith('ACCOUNT'):
                 env_file.write(k + '=' + v + '\n')
