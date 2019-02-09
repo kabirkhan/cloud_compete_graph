276547bc27f3f41b5244d2075bc9be3d7abdea07
Kabir Khan <kakh@microsoft.com>
2019-02-09 01:48:44 -0800

    CI

--- write_env.py
+++ write_env.py
@@ -4,6 +4,5 @@ import os
 if __name__ == '__main__':
     with open('.envasdf', 'w+') as env_file:
         for k, v in os.environ.items():
-            print(k)
             if k.startswith('AZURE') or k.startswith('COSMOS') or k.startswith('ACCOUNT'):
                 env_file.write(k + '=' + v + '\n')
